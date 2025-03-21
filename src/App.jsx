0da189baeaf6cb5c54602bf67f5b081cc3cc18da
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-21 22:54:08 +0000

    Modifing from <SignUp></SignUp> into <RegisterLogin></RegisterLogin>

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SignUp from './SignUp'
+import RegisterLogin from './Components/RegisterLogin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <SignUp>
+      <RegisterLogin>
 
-      </SignUp>
+      </RegisterLogin>
     </>
   )
 }
