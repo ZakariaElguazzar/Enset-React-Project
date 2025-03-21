870a9ed5c5b213c6fafbb6591578393ac88d19fd
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-21 17:59:04 +0000

    Init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SignUp from './SignUp'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <SignUp>
+
+      </SignUp>
+    </>
+  )
+}
+export default App
---
+++ src/SignUp.jsx
@@ -0,0 +1,18 @@
+function SignUp(){
+
+    return(
+    <>
+        <div className="">
+            <img src="" alt="" />
+            <form action="">
+                <input type="text" name="login" />
+                <input type="text" name="" id="name" />
+                <input type="text" name="" id="password" />
+
+            </form>
+        </div>
+    </>
+    )
+}
+
+export default SignUp
